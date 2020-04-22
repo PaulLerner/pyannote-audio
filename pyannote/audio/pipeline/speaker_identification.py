ebea2bbb33777a8a53c807cd4d37bf3b8f5e16ca
Paul Lerner <lerner@tuta.io>
2020-04-22 16:01:39 +0200

    feat: speaker identification pipeline using KNN classifier -> embeddings are no longer averaged by speaker

--- pyannote/audio/pipeline/speaker_identification.py
+++ pyannote/audio/pipeline/speaker_identification.py
@@ -41,7 +41,7 @@ from pyannote.metrics.identification import IdentificationErrorRate
 from .speech_turn_segmentation import SpeechTurnSegmentation
 from .speech_turn_segmentation import OracleSpeechTurnSegmentation
 
-from pyannote.pipeline.blocks.classification import ClosestAssignment
+from pyannote.pipeline.blocks.classification import ClosestAssignment, KNN
 
 from pyannote.pipeline import Pipeline
 from pyannote.pipeline.parameter import Uniform
@@ -87,7 +87,7 @@ class SpeakerIdentification(Pipeline):
         files provide the embeddings in the "emb" key.
     metric : {'euclidean', 'cosine', 'angular'}, optional
         Metric used for comparing embeddings. Defaults to 'cosine'.
-    evaluation_only : `bool`
+    evaluation_only : `bool`, optional
         Only process the evaluated regions. Default to False.
     """
 
@@ -214,3 +214,136 @@ class SpeakerIdentification(Pipeline):
         """Return new instance of diarization error rate metric"""
 
         return IdentificationErrorRate(collar=0.0, skip_overlap=False)
+
+
+class SpeakerIdentificationKNN(SpeakerIdentification):
+    """Speaker identification pipeline using k-nearest neighbors
+
+    Parameters
+    ----------
+    references : dict or Text
+        Either a dict like {identity : features}
+        or the name of a pyannote protocol which should be loaded like this.
+    subsets: set, optional
+        which protocol subset to get reference from.
+        Defaults to {'train'}
+    label_min_duration: float or int, optional
+        Only keep speaker with at least `label_min_duration` of annotated data.
+        Defaults to keep every speaker (i.e. 0.0)
+    sad_scores : Text or Path or 'oracle', optional
+        Describes how raw speech activity detection scores
+        should be obtained. It can be either the name of a torch.hub model, or
+        the path to the output of the validation step of a model trained
+        locally, or the path to scores precomputed on disk.
+        Defaults to "@sad_scores", indicating that protocol
+        files provide the scores in the corresponding "sad_scores" key.
+        Use 'oracle' to assume perfect speech activity detection.
+    scd_scores : Text or Path or 'oracle', optional
+        Describes how raw speaker change detection scores
+        should be obtained. It can be either the name of a torch.hub model, or
+        the path to the output of the validation step of a model trained
+        locally, or the path to scores precomputed on disk.
+        Defaults to "@scd_scores", indicating that protocol
+        files provide the scores in the corresponding "scd_scores" key.
+        Use 'oracle' to assume perfect speech turn segmentation,
+        `sad_scores` should then be set to 'oracle' too.
+    embedding : Text or Path, optional
+        Describes how raw speaker embeddings should be obtained. It can be
+        either the name of a torch.hub model, or the path to the output of the
+        validation step of a model trained locally, or the path to embeddings
+        precomputed on disk. Defaults to "@emb" that indicates that protocol
+        files provide the embeddings in the "emb" key.
+    metric : {'euclidean', 'cosine', 'angular'}, optional
+        Metric used for comparing embeddings. Defaults to 'cosine'.
+    evaluation_only : `bool`, optional
+        Only process the evaluated regions. Default to False.
+    """
+
+    def __init__(
+        self,
+        references : Union[dict, Text],
+        subsets : set = {'train'},
+        label_min_duration : Union[float, int] = 0.0,
+        sad_scores: Union[Text, Path] = None,
+        scd_scores: Union[Text, Path] = None,
+        embedding: Union[Text, Path] = None,
+        metric: Optional[str] = "cosine",
+        evaluation_only: Optional[bool] = False
+    ):
+
+        super().__init__(references, subsets, label_min_duration, sad_scores, scd_scores,
+                         embedding, metric, evaluation_only)
+        self.classifier = KNN(self.metric)
+
+    def __call__(self, current_file: dict, use_threshold: bool = True) -> Annotation:
+        """Apply speaker identification
+
+        Parameters
+        ----------
+        current_file : `dict`
+            File as provided by a pyannote.database protocol.
+        use_threshold : `bool`, optional
+            Ignores `closest_assignment.threshold` if False
+            -> sample embeddings are assigned to the closest target no matter the distance
+            Defaults to True.
+
+        Returns
+        -------
+        hypothesis : `pyannote.core.Annotation`
+            Speaker identification output.
+        """
+
+        # segmentation into speech turns
+        speech_turns = self.speech_turn_segmentation(current_file)
+
+        # some files are only partially annotated and therefore one cannot
+        # evaluate speaker diarization results on the whole file.
+        # this option simply avoids trying to cluster those
+        # (potentially messy) un-annotated refions by focusing only on
+        # speech turns contained in the annotated regions.
+        if self.evaluation_only:
+            annotated = get_annotated(current_file)
+            speech_turns = speech_turns.crop(annotated, mode="intersection")
+
+        # gather targets embedding
+        X_targets, targets_labels = [], []
+        for label, embeddings in self.references.items():
+            targets_labels.extend([label for _ in embeddings])
+            X_targets.extend(embeddings)
+
+        #gather inference embeddings
+        embedding = self._embedding(current_file)
+
+        # gather speech turns embedding
+        labels = speech_turns.labels()
+        X, assigned_labels, skipped_labels = [], [], []
+        for l, label in enumerate(labels):
+
+            timeline = speech_turns.label_timeline(label, copy=False)
+
+            # be more and more permissive until we have
+            # at least one embedding for current speech turn
+            for mode in ["strict", "center", "loose"]:
+                x = embedding.crop(timeline, mode=mode)
+                if len(x) > 0:
+                    break
+
+            # skip labels so small we don't have any embedding for it
+            if len(x) < 1:
+                skipped_labels.append(label)
+                continue
+
+            assigned_labels.append(label)
+
+            #average speech turn embeddings
+            X.append(np.mean(x, axis=0))
+
+        # assign speech turns to the nearest neighbor
+        assignments = self.classifier(np.vstack(X_targets),
+                                      np.vstack(X),
+                                      targets_labels,
+                                      use_threshold = use_threshold)
+        mapping = {label: target_label
+                   for label, target_label in zip(assigned_labels, assignments)}
+
+        return speech_turns.rename_labels(mapping=mapping)
