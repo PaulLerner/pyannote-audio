01029b333ba3fd8c575af1717f696df4d9069e08
Paul Lerner <lerner@tuta.io>
2020-06-03 12:41:40 +0200

    refactor: move transcription parsing to pyannote.database -> Convert spaCy Doc to Annotation

--- pyannote/audio/pipeline/ASR.py
+++ pyannote/audio/pipeline/ASR.py
@@ -26,9 +26,6 @@
 # AUTHORS
 # Paul LERNER
 
-from pathlib import Path
-from string import punctuation
-
 from pyannote.core import Annotation, Segment
 
 from pyannote.pipeline import Pipeline
@@ -41,86 +38,34 @@ NORMALIZED = "normalized"
 # normalized names we don't want to consider as annotated
 NA = {'UNKNOWN', 'multiple_persons'}
 
-def parse_aligned(annotations, uri):
-    """
-    Parameters
-    ----------
-    annotations: `str` or `Path`
-        path to the annotations.
-    uri : `str
-        uri of the file
-
-    Returns
-    -------
-    annotation: List[Tuple[Segment, str, float]]]
-    """
-    annotation = []
-    aligned = annotations / f'{uri}.aligned'
-
-    with open(aligned) as file:
-        aligned = file.read().split('\n')
-
-    for line in aligned:
-        if line == '':
-            continue
-        _, _, start, end, text, confidence = line.split()
-        start, end, confidence = map(float, (start, end, confidence))
-        segment = Segment(start, end)
-        annotation.append((segment, text, confidence))
-
-    return annotation
-
 class OracleASR(Pipeline):
-    """
-    Parameters
-    ----------
-    asr: `str` or `Path`
-        path to the ASR annotations.
-        only the '.aligned' format described in pyannote.db.plumcot
-        is supported for now
+    """Timestamped transcriptions.
 
     Hyperparameters
     ---------------
     confidence: `float`
         only keep words with a confidence above `threshold`
     """
-    def __init__(self, annotations, asr):
+    def __init__(self):
         super().__init__()
-        self.annotations = Path(annotations)
-        self.asr = Path(asr)
         self.confidence = Uniform(0, 1)
 
     def __call__(self, current_file: dict) -> Annotation:
-        """Parses annotations given file uri in an Annotation"""
+        """Convert spaCy Doc to Annotation"""
         uri = current_file['uri']
-        plain_text = parse_aligned(self.asr, uri)
         annotation = Annotation(uri, modality='text')
 
-        for segment, text, confidence in plain_text:
-            if confidence < self.confidence:
+        transcription = current_file['transcription']
+        for token in transcription:
+            if token._.alignment_confidence < self.confidence:
                 continue
-            annotation[segment, PLAIN] = text
+            segment = Segment(token._.time_start, token._.time_end)
+            annotation[segment, PLAIN] = token.text
 
         return annotation
 
-def strip_punct(string):
-    return string.translate(str.maketrans('', '', punctuation))
-
-def strip_dots(string):
-    return string.replace('.','')
-
 class OracleNormalizer(Pipeline):
-    """
-    Parameters
-    ----------
-    annotations: `str` or `Path`
-        path to the annotations.
-        only the '.csv' format described in pyannote.db.plumcot
-        is supported for now
-    asr: `str` or `Path`
-        path to the ASR annotations.
-        only the '.aligned' format described in pyannote.db.plumcot
-        is supported for now
+    """Timestamped transcriptions with normalized (proper) names on a separate track.
 
     Hyperparameters
     ---------------
@@ -128,75 +73,27 @@ class OracleNormalizer(Pipeline):
         only keep words with a confidence above `threshold`
     """
 
-    def __init__(self, annotations, asr):
+    def __init__(self):
         super().__init__()
-        self.annotations = Path(annotations)
-        self.asr = Path(asr)
         self.confidence = Uniform(0, 1)
 
     def __call__(self, current_file: dict) -> Annotation:
-        """Parses annotations given file uri in an Annotation"""
+        """Convert spaCy Doc to Annotation.
+        Only proper names are kept (i.e. pronouns are discarded).
+        Also, ambiguous labels such as 'UNKNOWN', 'multiple_persons' are discarded.
+        """
         uri = current_file['uri']
-        plain_text = parse_aligned(self.asr, uri)
         annotation = Annotation(uri, modality='text')
 
-        # 1. read entities csv
-        entities = self.annotations / f'merge_{uri}.csv'
-        with open(entities) as file:
-            entities = file.read().split('\n')
-
-        # 2. parse entities csv to list
-        normalized = []
-        for line in entities[1:]:
-            if line == '':
+        transcription = current_file['transcription']
+        for token in transcription:
+            if token._.alignment_confidence < self.confidence:
                 continue
-            _, _, token, _, pos, _, _, _, _, _, _, _, _, _, _, label = line.split(';')
-            token, pos, label = map(str.strip, (token, pos, label))
-            # remove empty lines
-            if token == '':
-                continue
-            # first token of each line includes speaker names
-            token = token[token.find(' ') + 1:]
-
-            normalized.append((token, pos, label))
-
-        # 3. merge entities with ASR
-        i = 0
-        for segment, text, confidence in plain_text:
-            skip_text, tokenization = False, False
-            token, pos, label = normalized[i]
-            # handle tokenization
-            while token != text and i+1 < len(normalized):
-                # handle weird '"' corner-case
-                if '"' in token:
-                    break
-                # handle punctuation
-                elif token== '.':
-                    i+=1
-                    token, pos, label = normalized[i]
-                elif strip_dots(token) == strip_dots(text):
-                    break
-                elif text == '.':
-                    skip_text = True
-                    break
-                # token was split by a tokenizer at some point
-                else:
-                    t, p, l = normalized[i+1]
-                    token += t
-                    pos += p
-                    label += l
-                    i += 1
-            if skip_text:
-                continue
-            i += 1
-
-            if confidence < self.confidence:
-                continue
-
-            annotation[segment, PLAIN] = text
-
+            segment = Segment(token._.time_start, token._.time_end)
+            annotation[segment, PLAIN] = token.text
+            label = token.ent_kb_id_
             # keep only proper names
-            if label != '' and pos == 'PROPN' and label not in NA:
+            if label != '' and token.pos_ == 'PROPN' and label not in NA:
                 annotation[segment, NORMALIZED] = label
 
         return annotation
